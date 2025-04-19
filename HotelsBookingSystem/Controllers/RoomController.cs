2d8e360155614c026811a6a40c779beeb32116ba
Alaa Elsisy <elsisyalaa0@gmail.com>
2025-04-19 18:57:27 +0200

    add rooms filter part

--- HotelsBookingSystem/Controllers/RoomController.cs
+++ HotelsBookingSystem/Controllers/RoomController.cs
@@ -1,32 +1,104 @@
 ﻿using HotelsBookingSystem.Models;
 using HotelsBookingSystem.Repository;
+using HotelsBookingSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using PagedList;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace HotelsBookingSystem.Controllers
 {
+
     public class RoomController : Controller
-    {
+    { 
         private readonly IRoomRepository roomRepository;
-        public RoomController(IRoomRepository roomRepo) {
+        public RoomController(IRoomRepository roomRepo)
+        {
             roomRepository = roomRepo;
         }
-        public IActionResult Index(int page =1 )
+
+        public IActionResult Index(int page = 1)
         {
-            int pageSize = 10;
-            IPagedList<Room> rooms = roomRepository.GetAll(page, pageSize);
-            return View(rooms);
+            int PageSize = 3;
+            var rooms = roomRepository.GetAllroom();
+
+            var roomViewModels = rooms.Select(r => new RoomViewModel
+            {
+                Id = r.Id,
+                Description = r.Description,
+                Type = r.Type,
+                Status = r.Status,
+                PricePerNight = r.PricePerNight,
+                RoomImages = r.RoomImages.Select(img => img.ImageUrl).ToList(),
+                hotel = r.Hotel,
+                hotels = roomRepository.GetAllhotels()
+            }).ToPagedList(page, PageSize);
+
+            return View(roomViewModels);
         }
 
+        #region detail
+        public IActionResult Detail(int id)
+        {
 
-        public IActionResult Detail(int id) { 
-            
             Room room = roomRepository.GetById(id);
-            if (room == null) {
+            if (room == null)
+            {
                 return NotFound();
             }
-           return View(room);
-        
+            return View(room);
+
         }
+        #endregion
+
+        #region filter
+
+        public IActionResult FilterRooms(
+             int page = 1,
+             string type = null,
+             int? minPrice = null,
+             int? maxPrice = null,
+             int? hotelId = null,
+             string city = null)
+        {
+            int PageSize = 3;
+            List<Hotel> hotellist = roomRepository.GetAllhotels();
+            IPagedList<Room> filteredRooms = roomRepository.FilterRooms(
+                type, minPrice, maxPrice, hotelId, city, page, PageSize);
+
+            
+            if (filteredRooms == null || !filteredRooms.Any())
+            {
+                var emptyRoomViewModel = new List<RoomViewModel>
+        {
+            new RoomViewModel { hotels = hotellist }
+        };
+
+                 
+                ViewBag.Message = "No rooms found matching your criteria.";
+
+                return View("Index", emptyRoomViewModel.ToPagedList(page, PageSize));
+            }
+
+            
+            var roomViewModels = filteredRooms.Select(r => new RoomViewModel
+            {
+                Id = r.Id,
+                Description = r.Description,
+                Type = r.Type,
+                Status = r.Status,
+                PricePerNight = r.PricePerNight,
+                RoomImages = r.RoomImages.Select(img => img.ImageUrl).ToList(),
+                hotel = r.Hotel,
+                hotels = hotellist, 
+            }).ToPagedList(page, PageSize);
+
+            return View("Index", roomViewModels);
+        }
+
+
+        #endregion
+
+
+
     }
 }
