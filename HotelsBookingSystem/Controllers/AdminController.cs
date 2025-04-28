c1742e991196a7b8c6ea1fd10de839b6045ba798
AhmedAbuAlmagd <abuelmagdahmed6@gmail.com>
2025-04-28 18:28:03 +0300

    Admin Updates

--- HotelsBookingSystem/Controllers/AdminController.cs
+++ HotelsBookingSystem/Controllers/AdminController.cs
@@ -25,18 +25,25 @@ namespace HotelsBookingSystem.Controllers
             return View(dashboardData);
         }
 
-        public IActionResult GetReviews(int hotelId = 0, int rating = 0, int page = 1)
+        public IActionResult GetReviews(string hotelId = "", string rating = "", int page = 1)
         {
-            ViewBag.Hotels = _HotelRepository.GetAllhotels().Where(x => x.Reviews.Count() != 0);
+            ViewBag.CurrentHotelId = hotelId;
+            ViewBag.CurrentRating = rating;
 
-            var reviews = _ReviewRepository.GetAll(hotelId, rating);
+            ViewBag.Hotels = _HotelRepository.GetAllhotels().Where(x => x.Reviews.Count() > 0);
+
+            int hotelIdValue = string.IsNullOrEmpty(hotelId) ? 0 : int.Parse(hotelId);
+            int ratingValue = string.IsNullOrEmpty(rating) ? 0 : int.Parse(rating);
+
+            var reviews = _ReviewRepository.GetAll(hotelIdValue, ratingValue);
 
             int totalItems = reviews.Count();
             int pageSize = 10;
             int totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
             page = Math.Max(1, Math.Min(page, Math.Max(1, totalPages)));
 
-                 reviews = reviews
+            reviews = reviews
                 .Skip((page - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
