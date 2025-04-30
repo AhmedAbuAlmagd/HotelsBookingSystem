4498dd0af9169881aedf517cb6751850cbb8dccb
Alaa Elsisy <elsisyalaa0@gmail.com>
2025-04-30 17:21:49 +0300

    add review vm

--- HotelsBookingSystem/Controllers/ReviewsController.cs
+++ HotelsBookingSystem/Controllers/ReviewsController.cs
@@ -2,45 +2,112 @@
 using HotelsBookingSystem.Repository;
 using HotelsBookingSystem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HotelsBookingSystem.Controllers
 {
+    [Authorize]
+
     public class ReviewsController : Controller
     {
         private readonly IReviewRepository reviewRepository;
         private readonly IHotelRepository hotelRepository;
-
-        public ReviewsController(IReviewRepository reviewRepo, IHotelRepository hotelRepo)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ReviewsController(IReviewRepository reviewRepo, IHotelRepository hotelRepo, UserManager<ApplicationUser> userManager)
         {
             reviewRepository = reviewRepo;
             hotelRepository = hotelRepo;
-
+            _userManager = userManager;
         }
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
-            var hotels = hotelRepository.GetHotelsWithRoomsAndImages();  
+
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                ViewData["Message"] = "Please log in to leave a review.";
+            }
+
+            var hotels = hotelRepository.GetHotelsWithRoomsAndImages();
             var model = new ReviewViewModel
             {
+                Comment = "",
+                HotelName = "",
+                UserName = "",
+                HotelId = 0,
+                Rating = 0,
+
                 hotels = hotels
             };
             return View(model);
-           
         }
 
-        //public IActionResult Create(int hotelId)
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create( ReviewViewModel reviewVM)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return RedirectToAction("Login", "Account");
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login", "Account"); 
+            }
+            int hotelid = reviewVM.HotelId ?? 0;    
+            var hotel = hotelRepository.GetById(hotelid);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.Identity.Name;
+            
+
+            var review = new Review
+            {
+
+                HotelId = hotelid,
+               
+                Comment = reviewVM.Comment,
+                Rating = reviewVM.Rating,
+                User = user,
+
+
+                Hotel = hotel,
+                UserId = userId,
+                
+            };
+            if (ModelState.IsValid)
+            {
+
+                reviewRepository.Add(review);
+                reviewRepository.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(review);
+             
+        }
+
+        
+        //[HttpPost]
+        //public IActionResult Create(Review review)
         //{
-        //    var hotel = hotelRepository.GetById(hotelId);
-        //    if (hotel == null)
+        //    if (ModelState.IsValid)
         //    {
-        //        return NotFound();
+               
+        //        reviewRepository.Add(review);
+        //        reviewRepository.SaveChanges();
+        //        return RedirectToAction("Index"); 
         //    }
-        //    var review = new Review
-        //    {
-        //        HotelId = hotelId,
-        //        Hotel = hotel
-        //    };
+
+           
         //    return View(review);
         //}
-        
     }
 }
--- HotelsBookingSystem/ViewModels/ReviewViewModel.cs
+++ HotelsBookingSystem/ViewModels/ReviewViewModel.cs
@@ -8,7 +8,13 @@ namespace HotelsBookingSystem.ViewModels
         public string? Comment { get; set; }
         public string? HotelName { get; set; }
         public string? UserName { get; set; }
+        
+        
+        // for dropdown
         public List<Hotel>? hotels { get; set; }
+        #region review
         public int? HotelId { get; set; }
+        public int? Rating { get; set; }
+        #endregion
     }
 }
